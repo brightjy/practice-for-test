878eccf8485ac34ee15ffc643ecb612a866ad3ef
박지영 <barkji0@naver.com>
2023-01-17 21:30:22 +0900

    최빈값 구하기

---
+++ FindMode.java
@@ -0,0 +1,22 @@
+public class FindMode {
+  public int solution(int[] array) {
+    int answer = 0;
+    int[] index = new int[1001];
+    int max = Integer.MIN_VALUE;
+    
+    for (int i=0; i<array.length; i++) {
+        index[array[i]]++;
+    }
+    
+    for (int i=0; i<index.length; i++) {
+        if(index[i] > max) {
+            max = index[i];
+            answer = i;
+        } else if (index[i] == max) {
+            answer = -1;
+        }
+    }
+            
+    return answer;
+  }
+}
